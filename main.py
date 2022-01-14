884d6baa55ccc1cecf4fb48346b53406a1236e2f
Sachin <sachinbhopiofficial@gmail.com>
2022-01-14 16:14:21 +0530

    though_postman method added

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
@@ -11,6 +11,33 @@ def fun1():
 
     return str(test3)
     # return "<h1> Result is : {}</h1>".format(test3)
+
+
+@app.route("/through_postman", methods=['POST'])
+def fun2():
+    if (request.method == 'POST'):
+        op = request.json['operation']
+        n1 = int(request.json['num1'])
+        n2 = int(request.json['num2'])
+
+        if (op == 'add'):
+            r = n1 + n2
+            result = op + " of " + str(n1) + " and " + str(n2) + " is " + str(r)
+
+        if (op == 'sub'):
+            r = n1 - n2
+            result = op + " of " + str(n1) + " and " + str(n2) + " is " + str(r)
+
+        if (op == 'mul'):
+            r = n1 * n2
+            result = op + " of " + str(n1) + " and " + str(n2) + " is " + str(r)
+
+        if (op == 'div'):
+            r = float(n1) / n2
+            result = op + " of " + str(n1) + " and " + str(n2) + " is " + str(r)
+
+        return jsonify(result)
+
 
 
 if __name__ == '__main__':
