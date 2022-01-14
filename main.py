d4cc05cc064b8203ee27ae16224c780eecf858b3
Sachin <sachinbhopiofficial@gmail.com>
2022-01-14 16:52:50 +0530

    though_html method added

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 
 app = Flask(__name__)
 
 
-@app.route("/sachin")
+@app.route("/through_addressbar")
 def fun1():
     test1 = int(request.args.get('val1'))
     test2 = int(request.args.get('val2'))
@@ -38,6 +38,34 @@ def fun2():
 
         return jsonify(result)
 
+@app.route('/', methods = ['GET',' POST'])
+def home():
+    return render_template('index.html')
+
+@app.route('/through_html', methods = ['POST'])
+def fun3():
+    if (request.method == 'POST'):
+        op = request.form['operator']
+        n1 = int(request.form['number1'])
+        n2 = int(request.form['number2'])
+
+        if (op == 'add'):
+            r = n1 + n2
+            res = op + " of " + str(n1) + " and " + str(n2) + " is " + str(r)
+
+        if (op == 'sub'):
+            r = n1 - n2
+            res = op + " of " + str(n1) + " and " + str(n2) + " is " + str(r)
+
+        if (op == 'mul'):
+            r = n1 * n2
+            res = op + " of " + str(n1) + " and " + str(n2) + " is " + str(r)
+
+        if (op == 'div'):
+            r = float(n1) / n2
+            res = op + " of " + str(n1) + " and " + str(n2) + " is " + str(r)
+
+        return render_template('result.html', result = str(res))
 
 
 if __name__ == '__main__':
